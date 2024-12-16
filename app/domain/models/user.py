09898f3ee89740bd63b7eebde935c895014401e9
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-12-16 19:00:59 +0800

    (feat): remove user permission link

--- app/domain/models/user.py
+++ app/domain/models/user.py
@@ -1,42 +1,44 @@
 from datetime import date, datetime
+from enum import Enum
 from typing import List, Optional
 
 from sqlmodel import Field, Relationship, SQLModel
 
 from app.domain.models.checkin import Checkin
 from app.domain.models.daily_summary import DailySummary
-from app.domain.models.permission import Permission
-from app.domain.models.user_permission import UserPermissionLink
+
+
+class PermissionEnum(str, Enum):
+	"""
+	Attributes:
+	    admin: admin
+	    user: user
+	"""
+
+	admin = 'admin'
+	user = 'user'
 
 
 # Users 表
 class User(SQLModel, table=True):
-    __tablename__ = "user"
+	__tablename__ = 'user'
 
-    id: int = Field(default=None, primary_key=True)
-    name: str
-    rfid: str = Field(unique=True)
-    position: Optional[str]  # 職位
-    department: Optional[str]  # 部門
-    hire_date: Optional[date]  # 入職日期
-    hourly_rate: float = Field(default=0)  # 時薪
-    created_datetime: datetime = Field(
-        default_factory=lambda: datetime.now(),
-        sa_column_kwargs={"server_default": "now()"},
-    )
+	id: int = Field(default=None, primary_key=True)
+	name: str
+	rfid: str = Field(unique=True)
+	position: Optional[str]  # 職位
+	department: Optional[str]  # 部門
+	hire_date: Optional[date]  # 入職日期
+	created_datetime: datetime = Field(
+		default_factory=lambda: datetime.now(),
+		sa_column_kwargs={'server_default': 'now()'},
+	)
 
-    checkins: List["Checkin"] = Relationship(back_populates="user")  # 打卡記錄
-    daily_summaries: List["DailySummary"] = Relationship(
-        back_populates="user"
-    )  # 每日工時總結
+	checkins: List['Checkin'] = Relationship(back_populates='user')  # 打卡記錄
+	daily_summaries: List['DailySummary'] = Relationship(
+		back_populates='user'
+	)  # 每日工時總結
 
-    permissions: List["Permission"] = Relationship(
-        link_model=UserPermissionLink,
-        back_populates="users",
-        sa_relationship_kwargs={
-            "primaryjoin": "User.id == UserPermissionLink.user_id",
-            "secondaryjoin": (
-                "Permission.id == UserPermissionLink.permission_id"
-            ),
-        },
-    )
+	permission: PermissionEnum = Field(
+		default=PermissionEnum.user, description='權限'
+	)
